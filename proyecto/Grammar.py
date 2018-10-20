e2dadc0d7bbd046db25395271e21c6c9d52e86ab
freddycra <freddy-jam@hotmail.com>
2018-10-20 17:06:03 -0600

    agregando compiler

--- proyecto/Grammar.py
+++ proyecto/Grammar.py
@@ -1,15 +1,12 @@
 
 class Grammar(object):
-    def __init__(self):
+    def __init__(self, rules, symbols = 'abcdefghijklmnopqrstuvwxyz0123456789',\
+    markers=[u'\u03b1',u'\u03b2',u'\u03b3',u'\u03b4'], vars = 'xywz'):
         super(Grammar, self).__init__()
-        self.rules = []
-
-    def addRule(self, rule):
-        if self.checkRule(rule):
-            self.rules.append(rule)
-
-    def checkRule(self, rule):
-        return rule is not None;
+        self.rules = rules
+        self.markers = markers
+        self.symbols = symbols
+        self.vars = vars
 
     def printInfo(self):
         print(self.rules)
